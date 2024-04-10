991b86bff25496613adb821348c2ba884fd92f48
Michael Hill <hillm5@jacks.sfasu.edu>
2024-04-09 22:06:11 -0500

    Zoom Recording

---
+++ ModernMoviesWeb/Pages/Model/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModernMoviesWeb.Pages.Model
+{
+	public class Movie
+	{
+		[Display(Name = "Movie ID")]
+		public int MovieID { get; set; }
+		[Required]
+		[Display(Name = "Movie Name")]
+		public string MovieName { get; set; }
+		[Display(Name = "Movie Description")]
+		public string MovieDesc { get; set;}
+		[Display(Name = "Movie Runtime (Minutes)")]
+		[Required]
+		public int MinRuntime { get; set; }
+		
+		[Required]
+		public int RatingID { get; set; }
+		
+		[Required]
+		public int GenreID { get; set; }
+		
+	}
+}
---
+++ ModernMoviesWeb/Pages/MovieAdmin/ViewMovies.cshtml.cs
@@ -0,0 +1,81 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
+using ModernMoviesBusiness;
+using ModernMoviesWeb.Pages.Model;
+
+namespace ModernMoviesWeb.Pages.MovieAdmin
+{
+	[BindProperties]
+    public class ViewMoviesModel : PageModel
+    {
+		public List<SelectListItem> Genres { get; set; } = new List<SelectListItem>();
+
+		public List<Movie> Movies { get; set; } = new List<Movie>();
+
+		public int SelectedGenreId { get; set; }
+        public void OnGet()
+        {
+			PopulateGenreDDL();
+        }
+
+		public void OnPost()
+		{
+			PopulateMovie(SelectedGenreId);
+			PopulateGenreDDL();
+		}
+
+		private void PopulateMovie(int id)
+		{
+			using(SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString()))
+			{
+				string cmdText = "SELECT MovieName, MovieDesc, MinRuntime, RatingID, GenreID, MovieID FROM Movie WHERE GenreID=@genreID";
+				SqlCommand cmd = new SqlCommand(cmdText, conn);
+				cmd.Parameters.AddWithValue("@genreID", id);
+				conn.Open();
+				SqlDataReader reader = cmd.ExecuteReader();
+				if(reader.HasRows)
+				{
+					while (reader.Read())
+					{
+						var movie = new Movie();
+						movie.MovieName = reader.GetString(0);
+						movie.MovieDesc = reader.GetString(1); 
+						movie.MinRuntime = reader.GetInt32(2);
+						movie.RatingID = reader.GetInt32(3);
+						movie.GenreID = reader.GetInt32(4);
+						movie.MovieID = reader.GetInt32(5);
+						Movies.Add(movie);
+					}
+				}
+			}
+		}
+
+		private void PopulateGenreDDL()
+		{
+			using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString()))
+			{
+				string cmdText = "SELECT GenreId, Genre FROM Genre ORDER BY Genre";
+				SqlCommand cmd = new SqlCommand(cmdText, conn);
+				conn.Open();
+				SqlDataReader reader = cmd.ExecuteReader();
+				if (reader.HasRows)
+				{
+					while (reader.Read())
+					{
+						var genre = new SelectListItem();
+						genre.Value = reader.GetInt32(0).ToString();
+						genre.Text = reader.GetString(1);
+						if(genre.Value == SelectedGenreId.ToString())
+						{
+							genre.Selected = true;
+						}
+						Genres.Add(genre);
+					}
+
+				}
+			}
+		}
+	}
+}
