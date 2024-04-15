5adc8a48f97ec0398f457e28b54237ab29a3c041
Michael Hill <hillm5@jacks.sfasu.edu>
2024-04-14 20:07:32 -0500

    movies page more

--- ModernMoviesWeb/Pages/MoviePages/Movies.cshtml.cs
+++ ModernMoviesWeb/Pages/MoviePages/Movies.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Data.SqlClient;
 using ModernMoviesBusiness;
+using ModernMoviesWeb.Pages.Model;
 
 namespace ModernMoviesWeb.Pages.MoviePages
 {
@@ -12,10 +13,12 @@ namespace ModernMoviesWeb.Pages.MoviePages
 	{
 		public List<String> Genres { get; set; } = new List<String>();
 		public List<String> Ratings { get; set; } = new List<String>();
+		public List<Movie> Movies { get; set; } = new List<Movie>();
 		public void OnGet()
 		{
 			PopulateGenreList();
 			PopulateRatingList();
+			PopulateMovie();
 		}
 
 		private void PopulateGenreList()
@@ -58,6 +61,31 @@ namespace ModernMoviesWeb.Pages.MoviePages
 						Ratings.Add(rating.Text);
 					}
 
+				}
+			}
+		}
+
+		private void PopulateMovie()
+		{
+			using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString()))
+			{
+				string cmdText = "SELECT MovieName, MovieDesc, MinRuntime, RatingID, GenreID, MovieID FROM Movie";
+				SqlCommand cmd = new SqlCommand(cmdText, conn);
+				conn.Open();
+				SqlDataReader reader = cmd.ExecuteReader();
+				if (reader.HasRows)
+				{
+					while (reader.Read())
+					{
+						var movie = new Movie();
+						movie.MovieName = reader.GetString(0);
+						movie.MovieDesc = reader.GetString(1);
+						movie.MinRuntime = reader.GetInt32(2);
+						movie.RatingID = reader.GetInt32(3);
+						movie.GenreID = reader.GetInt32(4);
+						movie.MovieID = reader.GetInt32(5);
+						Movies.Add(movie);
+					}
 				}
 			}
 		}
