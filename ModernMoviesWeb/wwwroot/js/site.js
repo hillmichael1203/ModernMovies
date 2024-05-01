7d5d480dd562985bc8f8660f403a1946e7afc9e9
DorianALP <dorianalittlecookperez@gmail.com>
2024-04-30 23:19:13 -0500

    CAROUSEL

--- ModernMoviesWeb/wwwroot/js/site.js
+++ ModernMoviesWeb/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $('#carouselExampleCaptions').find('.carousel-item').first().addClass('active');
+});
